19d335cd5a844ad3a1ff8d23c19d3c43b2a8f09d
Omar Saber <omar.saber@ligadigital.com>
2017-10-02 18:09:23 +0200

    templates

---
+++ USACO/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  g++ -std=c++11 -g -Wall -Wextra -Wshadow -Wformat=2 -Wfloat-equal -O2 main.cpp -o main
+else
+  g++ -std=c++11 -g -Wall -Wextra -Wshadow -Wformat=2 -Wfloat-equal -O2 "$1".cpp -o "$1"
+fi
---
+++ USACO/usaco_template.cpp
@@ -0,0 +1,17 @@
+/*
+ID: omar.se1
+LANG: C++11
+TASK: test
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("test.in", "r", stdin);
+  freopen("test.out", "w", stdout);
+  int a, b;
+  scanf("%d%d", &a, &b);
+  printf("%d\n", a + b);
+  return 0;
+}
