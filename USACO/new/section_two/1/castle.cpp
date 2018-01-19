9e7b7928c15769e7d345f39b026f3b98483c821f
Omar Saber <omar.saber@ligadigital.com>
2018-01-19 13:09:10 +0100

    improve castle implementation

--- USACO/new/section_two/1/castle.cpp
+++ USACO/new/section_two/1/castle.cpp
@@ -7,108 +7,94 @@ TASK: castle
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 55;
+#define SIZE(a) (int) (a).size()
 
 template<class T>
-inline T next() {
+inline T next()
+{
   T x;
   std::cin >> x;
   return x;
 }
 
-struct node {
+struct node
+{
   bool up, down;
   bool left, right;
   int x, y;
-  int component_size;
+  bool visited;
+  int size;
+  int component;
 
-  node() { }
+  node(): visited(false) { }
 
   node(int _x, int _y, int n):
     up((n & 2) == 0),
     down((n & 8) == 0),
     left((n & 1) == 0),
     right((n & 4) == 0),
-    x(_x), y(_y) { }
+    x(_x), y(_y),
+    visited(false)
+    {
+    }
 
-  void print() {
-    cout << "(" << x << ", " << y << ") = " 
+  vector<node*> dfs(vector<vector<node>>& grid)
+  {
+    if (visited) return {};
+    visited = true;
+    vector<node*> cur = {this};
+    vector<pair<int, int>> adj = {{x - up, y}, {x + down, y}, {x, y - left}, {x, y + right}};
+    for (const auto& p : adj) {
+      vector<node*> next = grid[p.first][p.second].dfs(grid);
+      cur.insert(end(cur), begin(next), end(next));
+    }
+    return cur;
+  }
+
+  void print() const
+  {
+    cerr << "(" << x << ", " << y << ") = " 
       << "(up = " << up << ", down = " << down 
       << ", left = " << left << ", right = " << right << ")\n";
   }
 };
 
-struct flood_fill {
-  int components;
-  int max_size;
-  int n, m;
-
-  vector<vector<bool>> visited;
-  array<array<node, N>, N> grid;
-  vector<vector<node>> connected;
-
-  flood_fill(int _n, int _m) {
-    components = 0;
-    max_size = numeric_limits<int>::min();
-    n = _n, m = _m;
-    visited = vector<vector<bool>>(n, vector<bool>(m, false));
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        grid[i][j] = node(i, j, next<int>());
-      }
-    }
-  }
-
-  int dfs(const node& cur) {
-    const int x = cur.x;
-    const int y = cur.y;
-    if (x < 0 || x >= n || y < 0 || y >= m) return 0;
-    if (visited[x][y]) return 0;
-    visited[x][y] = true;
-    connected[components].push_back(cur);
-    const node& value = grid[x][y];
-    int count = 1;
-    count += dfs(grid[x - value.up][y]);
-    count += dfs(grid[x + value.down][y]);
-    count += dfs(grid[x][y - value.left]);
-    count += dfs(grid[x][y + value.right]);
-    return count;
-  }
-
-  void do_it() {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (!visited[i][j]) {
-          connected.push_back(vector<node>());
-          int count = dfs(grid[i][j]);
-          max_size = max(max_size, count);
-          for (node& n : connected[components]) {
-            n.component_size = count;
-          }
-          components++;
-        }
-      }
-    }
-  }
-
-  void print_components() {
-    for (auto component : connected) {
-      for (int i = 0; i < (int) component.size(); i++) {
-        component[i].print();
-      }
-      cout << '\n';
-    }
-  }
-};
-
-int main() {
+int main()
+{
   //freopen("castle.in", "r", stdin);
   //freopen("castle.out", "w", stdout);
   int m, n;
   cin >> m >> n;
-  flood_fill solver(n, m);
-  solver.do_it();
-  solver.print_components();
-  cout << solver.components << '\n';
-  cout << solver.max_size << '\n';
+  vector<vector<node>> grid(n, vector<node>(m));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      grid[i][j] = node(i, j, next<int>());
+    }
+  }
+  int result = 0;
+  int max_size = numeric_limits<int>::min();
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      node& room = grid[i][j];
+      if (!room.visited) {
+        vector<node*> component = room.dfs(grid);
+        for (node* cn : component) {
+          cn->size = SIZE(component);
+          cn->component = result;
+        }
+        max_size = max(max_size, SIZE(component));
+        result++;
+      }
+    }
+  }
+  int x = -1, y = -1;
+  int merge_size = numeric_limits<int>::min();
+  char wall_direction = '-';
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      
+    }
+  }
+  cout << result << '\n';
+  cout << max_size << '\n';
 }
